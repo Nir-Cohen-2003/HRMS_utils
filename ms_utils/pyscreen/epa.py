22d387c2b53a19f4b3ae3857e45272133f5d4765
Nir <>
2025-07-10 12:47:34 +0300

    added ability to create the epa suspect list from dsstox dump and the short lists. need to polish this, and to standartuze the format of the resulting parquet file.

--- ms_utils/pyscreen/epa.py
+++ ms_utils/pyscreen/epa.py
@@ -2,7 +2,9 @@ from dataclasses import dataclass
 import polars as pl
 from pathlib import Path
 from typing import Iterable, Tuple
-from ms_utils.formats.epa_xlsx import read_xlsx_EPA_list_file
+from ..formats.epa_xlsx import read_xlsx_EPA_list_file_full_format, read_file_idetifiers_only, read_xlsx_EPA_list_file_short_format
+import requests
+import zipfile
 
 @dataclass
 class suspect_list_config:
@@ -102,11 +104,12 @@ def get_EPA(config:suspect_list_config,)-> pl.DataFrame:
     return EPA
 
 
-# TODO: add the function for construction of the suspect list. might add a GUI for it later.
-# takes an iterable of tuples (name, haz_level) and returns a polars DataFrame, which can be written to disk or used directly.
-def construct_suspect_list(list_dir:str|Path,lists:Iterable[Tuple[str,int]]) -> pl.DataFrame:
+def construct_suspect_list_from_full_lists(
+        list_dir:str|Path,
+        lists:Iterable[Tuple[str,int]]) -> pl.DataFrame:
     '''
     Constructs a suspect list from a directory containing Excel files with the given names and hazard levels.
+    the lists should be excel files with at least the sheet "Main Data" and "Synonym Identifier" in the format of the EPA lists, as opbtained usign the batch serach and export in the comptox dashboard. if you only have the short format lists, you can use the `construct_suspect_list_from_short_lists` function instead, togehther with the dsstox dump files, from which the synonyms can be extracted. an helper function to download the DSSTox dump file is also provided, as `download_DSS_Tox_dump_zip`.
     Args:
         list_dir (str|Path): The directory containing the Excel files.
         lists (Iterable[Tuple[str, int]]): An iterable of tuples, where each tuple contains a name (str) and a hazard level (int).
@@ -151,7 +154,7 @@ def construct_suspect_list(list_dir:str|Path,lists:Iterable[Tuple[str,int]]) -> 
     suspect_list = []
     for name, haz_level in lists:
         file_path = list_dir / f"{name}.xlsx"
-        df = read_xlsx_EPA_list_file(file_path)
+        df = read_xlsx_EPA_list_file_full_format(file_path)
         if df.is_empty():
             print(f"Warning: {file_path} is empty, skipping.")
             continue
@@ -163,6 +166,87 @@ def construct_suspect_list(list_dir:str|Path,lists:Iterable[Tuple[str,int]]) -> 
     suspect_list_df = suspect_list_df.sort('Haz_level',descending=False).unique(subset='DTXSID', keep='first')
     return suspect_list_df
 
+def construct_suspect_list_from_short_lists(
+        list_dir:str|Path,
+        lists:Iterable[Tuple[str,int]],
+        epa_dump_dir:Path|str|None,
+        ) -> pl.DataFrame:
+    """ Constructs a suspect list from a directory containing short format Excel files with the given names and hazard levels."""
+    list_dir = Path(list_dir)
+    assert list_dir.exists(), f"Directory {list_dir} does not exist"
+    assert list_dir.is_dir(), f"{list_dir} is not a directory"
+    assert lists, "The lists parameter is empty. Please provide a non-empty iterable of tuples (name, haz_level)."
+    assert all(isinstance(item, tuple) and len(item) == 2 for item in lists), "All items in the lists parameter must be tuples of (name, haz_level)."
+    assert all(isinstance(name, str) and isinstance(haz_level, int) for name, haz_level in lists), "Each tuple in the lists parameter must contain a string (name) and an integer (haz_level)."
+    assert all(0 <= haz_level <= 5 for _, haz_level in lists), "Haz_level must be an integer between 0 and 5 (inclusive)."
+
+    # Accept both with .xlsx and without .xlsx
+    list_paths = [(name if name.endswith('.xlsx') else f"{name}.xlsx", haz_level) for name, haz_level in lists]
+    list_paths = [list_dir / name for name, _ in list_paths]
+    assert all(path.exists() for path in list_paths), f"Files {[path for path in list_paths if not path.exists()]} do not exist in the directory {list_dir}"
+
+    # epa_dump_dir is required
+    assert epa_dump_dir is not None, "epa_dump_dir must be provided for short lists."
+    epa_dump_dir = Path(epa_dump_dir)
+    assert epa_dump_dir.exists(), f"epa_dump_dir {epa_dump_dir} does not exist"
+    assert epa_dump_dir.is_dir(), f"{epa_dump_dir} is not a directory"
+
+    # Read the DSSTox dump files, using read_file_idetifiers_only
+    dsstox_files = list(epa_dump_dir.glob('DSSToxDump*.xlsx'))
+    assert dsstox_files, f"No DSSTox dump files found in {epa_dump_dir}. Please provide the directory containing the DSSTox dump files."
+    dsstox = pl.concat(
+        [read_file_idetifiers_only(file_path) for file_path in dsstox_files],
+        how='vertical'
+    ).unique(subset='DTXSID')
+
+
+    # read the list files and add the haz_level to the DataFrame. read using read_xlsx_EPA_list_file_short_format
+    suspect_list = []
+    for name, haz_level in lists:
+        file_path = list_dir / f"{name}.xlsx"
+        df = read_xlsx_EPA_list_file_short_format(file_path)
+        if df.is_empty():
+            print(f"Warning: {file_path} is empty, skipping.")
+            continue
+        # join with the dsstox to get the synonyms and inchikey
+        df = df.join(dsstox, on='DTXSID', how='left')
+        # add the haz_level column
+        df = df.with_columns(pl.lit(haz_level).alias('Haz_level'))
+        suspect_list.append(df)
+
+    suspect_list_df = pl.concat(suspect_list, how='vertical')
+    suspect_list_df = suspect_list_df.sort('Haz_level', descending=False).unique(subset='DTXSID', keep='first')
+    return suspect_list_df
+    
+
+def download_DSSTox_dump(
+        output_dir:Path|str,
+        url:str='https://clowder.edap-cluster.com/files/6616d8d7e4b063812d70fc95') -> None:
+    '''
+    Downloads the DSSTox Dump file from the EPA website and saves it to the specified output path.
+    Args:
+        output_path (Path|str): The path where the downloaded file will be saved.
+        url (str): The URL of the DSSTox Dump file. Default is the EPA's DSSTox Dump fule url, for february 2024.
+    Returns:
+        None
+    '''
+    output_dir = Path(output_dir)
+    # make sure the output directory exists, by creating it if it doesn't
+    output_dir.mkdir(parents=True, exist_ok=True)
+    
+    # get the zip from the url
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise ConnectionError(f"Failed to download the file from {url}. Status code: {response.status_code}")
+    # save the zip file
+    zip_file_path = output_dir.joinpath('DSSToxDump.zip')
+    with open(zip_file_path, 'wb') as file:
+        file.write(response.content)
+    print(f"Downloaded DSSTox Dump file to {zip_file_path}")
+    # extract the zip file
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(output_dir)
+    print(f"Extracted DSSTox Dump file to {output_dir}")
 
 if __name__ == "__main__":
     
@@ -186,7 +270,7 @@ if __name__ == "__main__":
         ('GHS Skin and Eye  II', 3),
         # ("Chemical List MZCLOUD0722-2025-07-03", 0),
     ]
-    suspect_list = construct_suspect_list('/home/analytit_admin/Data/EPA/EPA_lists_full_format/', list_of_lists)
+    suspect_list = construct_suspect_list_from_full_lists('/home/analytit_admin/Data/EPA/EPA_lists_full_format/', list_of_lists)
     print(suspect_list.head())
     suspect_list.write_parquet('/home/analytit_admin/Data/EPA/suspect_list.parquet')
     # Example usage
@@ -195,3 +279,8 @@ if __name__ == "__main__":
     )
     EPA_df = get_EPA(config)
     print(EPA_df.head())
+
+
+    # dsstox = pl.read_parquet('/home/analytit_admin/Data/EPA/DSSTox.parquet')
+    # print(dsstox)
+    # print(dsstox.schema)
