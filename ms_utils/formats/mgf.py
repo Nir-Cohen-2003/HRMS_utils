70e96e31a38bf1d976f70549b7fa20d700bc8be2
Nir <>
2025-07-14 18:52:03 +0300

    combined it all to one function, since the splitting wasn't maling sense. might split the code in a better way in the future, if nthe need arises

--- ms_utils/formats/mgf.py
+++ ms_utils/formats/mgf.py
@@ -1,17 +1,20 @@
 import polars as pl
 from pathlib import Path
 import re
+from typing import Iterable
 from ..formula_annotation.utils import formula_to_array
 
-def split_mgf_entries(mgf_text: str) -> list[str]:
-    # Split on BEGIN IONS ... END IONS blocks
-    entries = re.findall(r'BEGIN IONS(.*?)END IONS', mgf_text, re.DOTALL)
-    return [entry.strip() for entry in entries if entry.strip()]
-
-def mgf_entries_to_dataframe(entries: list[str]) -> pl.DataFrame:
+def read_mgf_to_dataframe(
+        mgf_path: str | Path,
+        includes_MSn: bool = False
+        
+        ) -> pl.DataFrame:
+    with open(mgf_path, 'r') as f:
+        mgf_text = f.read()
+        entries = re.findall(r'BEGIN IONS(.*?)END IONS', mgf_text, re.DOTALL)
     # Put entries in a DataFrame
     df = pl.DataFrame({'entry': entries})
-
+    del entries  # Free up memory
     # Extract all key=value metadata using regex
     meta_keys = [
         "NAME", "DESCRIPTION", "EXACTMASS", "FORMULA", "INCHI", "INCHIAUX", "SMILES",
@@ -88,34 +91,16 @@ def mgf_entries_to_dataframe(entries: list[str]) -> pl.DataFrame:
     )
 
     return df
-
-def read_mgf_to_dataframe(mgf_path: str | Path) -> pl.DataFrame:
-    with open(mgf_path, 'r') as f:
-        mgf_text = f.read()
-    entries = split_mgf_entries(mgf_text)
-    return mgf_entries_to_dataframe(entries)
-
 # Example usage:
 if __name__ == "__main__":
     from time import perf_counter
     start_time = perf_counter()
-    mgf_file = Path("/home/analytit_admin/Downloads/20241003_enamdisc_neg_ms2.mgf")
+    mgf_file = Path("/home/analytit_admin/Downloads/20241003_enamdisc_neg_msn.mgf")
     df = read_mgf_to_dataframe(mgf_file)
     end_time = perf_counter()
-    print(df.head())
-    # print only the casted columns
-    print(df.select([
-        pl.col("EXACTMASS"),
-        pl.col("RTINSECONDS"),
-        pl.col("PEPMASS"),
-        pl.col("CHARGE"),
-        pl.col("FEATURE_MS1_HEIGHT"),
-        pl.col("MSLEVEL"),
-        pl.col("ISOLATION_WINDOW"),
-        pl.col("Num peaks"),
-        pl.col("collision_energy_mean"),
-        pl.col("multiple_collision_energies")
-    ]))
+    print(df)
+    # what is the MSLEVEL values, thier distributoion etc
+    print(df["MSLEVEL"].value_counts(sort=True))
     print(f"Time taken: {end_time - start_time:.2f} seconds")
     print(f"Number of entries: {df.height}")
     print(f'time per entry: {(end_time - start_time) / df.height:.8f} seconds')
