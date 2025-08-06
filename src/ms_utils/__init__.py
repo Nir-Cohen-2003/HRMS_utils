4ca488f807656ce27c6d8ebdddee8debf8b76542
Nir <>
2025-08-06 08:54:18 +0300

    starting to understand how to build a python package

--- src/ms_utils/__init__.py
+++ src/ms_utils/__init__.py
@@ -1,7 +1,11 @@
-from .rdkit import FingerprintParams, get_fp_list, get_fp_polars
 
-__all__ = [
-    "FingerprintParams",
-    "get_fp_list",
-    "get_fp_polars",
-]
+from .rdkit.fingerprint import FingerprintParams, get_fp_list, get_fp_polars
+
+# __all__ = [
+#     "FingerprintParams",
+#     "get_fp_list",
+#     "get_fp_polars",
+# ]
+
+if __name__ == "__main__":
+    print("This is the ms_utils package. Import submodules to use functionalities.")
