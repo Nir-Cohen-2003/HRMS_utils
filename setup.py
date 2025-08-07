87541a4f06fb04f6b0f9c28177e18ebac9d439cd
Nir <>
2025-08-07 15:00:29 +0300

    added giyhub actions so that it will build my wheels

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 import numpy
 import os
+import sys
 
 # --- Compilation settings from the nested setup.py ---
 
@@ -12,17 +13,21 @@ ext_base_path = "src/ms_utils/formula_annotation/mass_decomposition_impl"
 openmp_compile_args = []
 openmp_link_args = []
 
-# Detect compiler and set appropriate OpenMP flags
-if os.name == 'nt':  # Windows
+# C++ compile flags
+cpp_compile_args = ['-std=c++11', '-O3']
+
+# Detect compiler and set appropriate flags for the TARGET platform
+# sys.platform is patched by conda-build to reflect the target platform
+is_windows = sys.platform.startswith('win')
+
+if is_windows:
     openmp_compile_args = ['/openmp']
+    # -march=native is not applicable for cross-compilation
 else:  # Unix-like systems
     openmp_compile_args = ['-fopenmp']
     openmp_link_args = ['-fopenmp']
+    cpp_compile_args.extend(['-march=native', '-ffast-math', '-funroll-loops'])
 
-# C++ compile flags
-cpp_compile_args = ['-std=c++11', '-O3', '-march=native']
-if os.name != 'nt':
-    cpp_compile_args.extend(['-ffast-math', '-funroll-loops'])
 
 # --- Define the extension ---
 
