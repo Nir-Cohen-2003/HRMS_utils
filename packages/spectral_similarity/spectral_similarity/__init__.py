2cde7f192597187755040a05c1bb755589a0055e
Nir <>
2025-10-20 12:48:55 +0300

    when manully installing the whell, all works- hence the rust and python stuff are probably correct.

--- packages/spectral_similarity/spectral_similarity/__init__.py
+++ packages/spectral_similarity/spectral_similarity/__init__.py
@@ -6,10 +6,10 @@ from typing import TYPE_CHECKING
 import polars as pl
 from polars.plugins import register_plugin_function
 
-from spectral_similarity._internal import __version__ as __version__
+from ._internal import __version__ as __version__
 
 if TYPE_CHECKING:
-    from spectral_similarity.typing import IntoExprColumn
+    from .typing import IntoExprColumn
 
 LIB = Path(__file__).parent
 
