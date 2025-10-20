0e0de8aebb4c8846c3885541f91d088268a35351
Nir <>
2025-10-20 11:31:37 +0300

    the buils script was disabled for some reason. still, the rust compilation ran, need to understand why.

--- packages/spectral_similarity/spectral_similarity/__init__.py
+++ packages/spectral_similarity/spectral_similarity/__init__.py
@@ -18,8 +18,8 @@ def calculate_similarity(spectra: pl.Expr) -> pl.Expr:
     Calculate spectral similarity between two spectra.
     """
     return pl.plugins.register_plugin_function(
+        args=[spectra],
         plugin_path=LIB,
         function_name="calculate_similarity_struct",
-        args=[spectra],
         is_elementwise=True,
     )
