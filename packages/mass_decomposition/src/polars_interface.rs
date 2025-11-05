3fba0395578adaa8a283f6564496004b616717cc
Nir <>
2025-11-05 10:01:24 +0200

    moved becnhmark to python level, added rayon, need to fix interface in spectrum

--- packages/mass_decomposition/src/polars_interface.rs
+++ packages/mass_decomposition/src/polars_interface.rs
@@ -1,4 +1,5 @@
 use polars::prelude::*;
+use rayon::prelude::*;
 use pyo3_polars::derive::polars_expr;
 use crate::algorithms::{MassDecomposer, SpectrumDecomposer};
 use crate::common::{DecompositionParams, DecompositionKwargs, SpectrumDecompositionParams, NUM_ELEMENTS, Formula};
@@ -28,7 +29,7 @@ pub fn decompose_mass(inputs: &[Series], kwargs: DecompositionParams) -> PolarsR
         "decomposed_formulas".into(),
     );
 
-    for opt_mass in ca.into_iter() {
+    let results: PolarsResult<Vec<_>> = ca.into_iter().par_bridge().map(|opt_mass| {
         if let Some(mass) = opt_mass {
             let mut decomposer = MassDecomposer::new(min_bounds, max_bounds);
             let formulas = decomposer.decompose(mass, &kwargs);
@@ -49,12 +50,20 @@ pub fn decompose_mass(inputs: &[Series], kwargs: DecompositionParams) -> PolarsR
             // 2. Cast to Array(Int32, NUM_ELEMENTS)
             let array_series = list_series.cast(&DataType::Array(Box::new(DataType::Int32), NUM_ELEMENTS))?;
 
-            // 3. Append to the main list_builder
-            list_builder.append_series(&array_series)?;
+            Ok(Some(array_series))
+        } else {
+            Ok(None)
+        }
+    }).collect();
+
+    for res in results? {
+        if let Some(series) = res {
+            list_builder.append_series(&series)?;
         } else {
             list_builder.append_null();
         }
     }
+
 
     let out = list_builder.finish();
     Ok(out.into_series())
@@ -80,56 +89,61 @@ pub fn decompose_mass_with_bounds_struct(inputs: &[Series], kwargs: Decompositio
         "decomposed_formulas".into(),
     );
 
-    let mass_iter = mass_ca.into_iter();
-    let min_bounds_iter = min_bounds_ca.into_iter();
-    let max_bounds_iter = max_bounds_ca.into_iter();
+    let results: PolarsResult<Vec<_>> = izip!(mass_ca.into_iter(), min_bounds_ca.into_iter(), max_bounds_ca.into_iter())
+        .par_bridge()
+        .map(|(opt_mass, opt_min_bounds, opt_max_bounds)| {
+            if let (Some(mass), Some(min_bounds_arr), Some(max_bounds_arr)) = (opt_mass, opt_min_bounds, opt_max_bounds) {
+                
+                let mut min_bounds: Formula = [0; NUM_ELEMENTS];
+                let s_min = Series::from(min_bounds_arr);
+                let ca_min = s_min.i32()?;
+                ca_min.into_no_null_iter().enumerate().for_each(|(i, v)| {
+                    min_bounds[i] = v;
+                });
 
-    for (opt_mass, opt_min_bounds, opt_max_bounds) in izip!(mass_iter, min_bounds_iter, max_bounds_iter) {
-        if let (Some(mass), Some(min_bounds_arr), Some(max_bounds_arr)) = (opt_mass, opt_min_bounds, opt_max_bounds) {
-            
-            let mut min_bounds: Formula = [0; NUM_ELEMENTS];
-            let s_min = Series::from(min_bounds_arr);
-            let ca_min = s_min.i32()?;
-            ca_min.into_no_null_iter().enumerate().for_each(|(i, v)| {
-                min_bounds[i] = v;
-            });
+                let mut max_bounds: Formula = [0; NUM_ELEMENTS];
+                let s_max = Series::from(max_bounds_arr);
+                let ca_max = s_max.i32()?;
+                ca_max.into_no_null_iter().enumerate().for_each(|(i, v)| {
+                    max_bounds[i] = v;
+                });
 
-            let mut max_bounds: Formula = [0; NUM_ELEMENTS];
-            let s_max = Series::from(max_bounds_arr);
-            let ca_max = s_max.i32()?;
-            ca_max.into_no_null_iter().enumerate().for_each(|(i, v)| {
-                max_bounds[i] = v;
-            });
+                let mut decomposer = MassDecomposer::new(min_bounds, max_bounds);
+                let params = DecompositionParams {
+                    tolerance_ppm: kwargs.tolerance_ppm,
+                    min_dbe: kwargs.min_dbe,
+                    max_dbe: kwargs.max_dbe,
+                    dbe_mode: kwargs.dbe_mode.clone(),
+                    min_bounds,
+                    max_bounds,
+                };
+                let formulas = decomposer.decompose(mass, &params);
 
-            let mut decomposer = MassDecomposer::new(min_bounds, max_bounds);
-            let params = DecompositionParams {
-                tolerance_ppm: kwargs.tolerance_ppm,
-                min_dbe: kwargs.min_dbe,
-                max_dbe: kwargs.max_dbe,
-                dbe_mode: kwargs.dbe_mode.clone(),
-                min_bounds,
-                max_bounds,
-            };
-            let formulas = decomposer.decompose(mass, &params);
+                // 1. Build a List(Int32) series for the formulas of this mass
+                let mut list_primitive_builder = ListPrimitiveChunkedBuilder::<Int32Type>::new(
+                    "formulas_for_mass".into(),
+                    formulas.len(), // capacity
+                    formulas.len() * NUM_ELEMENTS, // values capacity
+                    DataType::Int32,
+                );
+                for formula in &formulas {
+                    list_primitive_builder.append_slice(formula);
+                }
+                let list_primitive_chunked = list_primitive_builder.finish();
+                let list_series = list_primitive_chunked.into_series();
 
-            // 1. Build a List(Int32) series for the formulas of this mass
-            let mut list_primitive_builder = ListPrimitiveChunkedBuilder::<Int32Type>::new(
-                "formulas_for_mass".into(),
-                formulas.len(), // capacity
-                formulas.len() * NUM_ELEMENTS, // values capacity
-                DataType::Int32,
-            );
-            for formula in &formulas {
-                list_primitive_builder.append_slice(formula);
+                // 2. Cast to Array(Int32, NUM_ELEMENTS)
+                let array_series = list_series.cast(&DataType::Array(Box::new(DataType::Int32), NUM_ELEMENTS))?;
+
+                Ok(Some(array_series))
+            } else {
+                Ok(None)
             }
-            let list_primitive_chunked = list_primitive_builder.finish();
-            let list_series = list_primitive_chunked.into_series();
+        }).collect();
 
-            // 2. Cast to Array(Int32, NUM_ELEMENTS)
-            let array_series = list_series.cast(&DataType::Array(Box::new(DataType::Int32), NUM_ELEMENTS))?;
-
-            // 3. Append to the main list_builder
-            list_builder.append_series(&array_series)?;
+    for res in results? {
+        if let Some(series) = res {
+            list_builder.append_series(&series)?;
         } else {
             list_builder.append_null();
         }
@@ -165,50 +179,57 @@ pub fn decompose_spectrum_with_precursor_struct(inputs: &[Series], kwargs: Spect
         "decomposed_spectrum".into(),
     );
 
-    let mz_iter = mz_ca.into_iter();
-    let precursor_iter = precursor_formula_ca.into_iter();
+    let results: PolarsResult<Vec<_>> = mz_ca.into_iter().zip(precursor_formula_ca.into_iter())
+        .par_bridge()
+        .map(|(mz_values_opt, precursor_formula_opt)| {
+            if let (Some(mz_values_s), Some(precursor_formula_arr)) = (mz_values_opt, precursor_formula_opt) {
+                let mz_values_ca = mz_values_s.f64()?;
+                let mz_values: Vec<f64> = mz_values_ca.into_no_null_iter().collect();
 
-    for (mz_values_opt, precursor_formula_opt) in mz_iter.zip(precursor_iter) {
-        if let (Some(mz_values_s), Some(precursor_formula_arr)) = (mz_values_opt, precursor_formula_opt) {
-            let mz_values_ca = mz_values_s.f64()?;
-            let mz_values: Vec<f64> = mz_values_ca.into_no_null_iter().collect();
-
-            let mut precursor_formula: Formula = [0; NUM_ELEMENTS];
-            let s = Series::from(precursor_formula_arr);
-            let ca = s.i32()?;
-            ca.into_no_null_iter().enumerate().for_each(|(i, v)| {
-                precursor_formula[i] = v;
-            });
+                let mut precursor_formula: Formula = [0; NUM_ELEMENTS];
+                let s = Series::from(precursor_formula_arr);
+                let ca = s.i32()?;
+                ca.into_no_null_iter().enumerate().for_each(|(i, v)| {
+                    precursor_formula[i] = v;
+                });
 
 
-            let mut decomposer = SpectrumDecomposer::new();
-            let formulas_per_mz = decomposer.decompose_spectrum_with_precursor(&mz_values, &precursor_formula, &kwargs);
+                let mut decomposer = SpectrumDecomposer::new();
+                let formulas_per_mz = decomposer.decompose_spectrum_with_precursor(&mz_values, &precursor_formula, &kwargs);
 
-            let mut inner_list_builder = get_list_builder(
-                &DataType::Array(Box::new(DataType::Int32), NUM_ELEMENTS),
-                formulas_per_mz.len(),
-                formulas_per_mz.iter().map(|f| f.len()).sum::<usize>(),
-                "formulas_for_mz".into(),
-            );
-
-            for formulas in formulas_per_mz {
-                let mut list_primitive_builder = ListPrimitiveChunkedBuilder::<Int32Type>::new(
-                    "formulas".into(),
-                    formulas.len(),
-                    formulas.len() * NUM_ELEMENTS,
-                    DataType::Int32,
+                let mut inner_list_builder = get_list_builder(
+                    &DataType::List(Box::new(DataType::Array(Box::new(DataType::Int32), NUM_ELEMENTS))),
+                    formulas_per_mz.len(),
+                    formulas_per_mz.iter().map(|f| f.len()).sum::<usize>(),
+                    "formulas_for_mz".into(),
                 );
-                for formula in &formulas {
-                    list_primitive_builder.append_slice(formula);
-                }
-                let list_primitive_chunked = list_primitive_builder.finish();
-                let list_series = list_primitive_chunked.into_series();
-                let array_series = list_series.cast(&DataType::Array(Box::new(DataType::Int32), NUM_ELEMENTS))?;
-                inner_list_builder.append_series(&array_series)?;
-            }
-            let inner_list_series = inner_list_builder.finish().into_series();
-            outer_list_builder.append_series(&inner_list_series)?;
 
+                for formulas in formulas_per_mz {
+                    let mut list_primitive_builder = ListPrimitiveChunkedBuilder::<Int32Type>::new(
+                        "formulas".into(),
+                        formulas.len(),
+                        formulas.len() * NUM_ELEMENTS,
+                        DataType::Int32,
+                    );
+                    for formula in &formulas {
+                        list_primitive_builder.append_slice(formula);
+                    }
+                    let list_primitive_chunked = list_primitive_builder.finish();
+                    let list_series = list_primitive_chunked.into_series();
+                    let array_series = list_series.cast(&DataType::Array(Box::new(DataType::Int32), NUM_ELEMENTS))?;
+                    inner_list_builder.append_series(&array_series)?;
+                }
+                let inner_list_series = inner_list_builder.finish().into_series();
+                Ok(Some(inner_list_series))
+
+            } else {
+                Ok(None)
+            }
+        }).collect();
+
+    for res in results? {
+        if let Some(series) = res {
+            outer_list_builder.append_series(&series)?;
         } else {
             outer_list_builder.append_null();
         }
