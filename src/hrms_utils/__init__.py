90fd71bb22b6188c9be29518a8e2e0bca8200264
Nir <>
2025-11-25 15:33:10 +0200

    fixed import issue

--- src/hrms_utils/__init__.py
+++ src/hrms_utils/__init__.py
@@ -1,4 +1,4 @@
-from . import rdkit, formula_annotation, formats
+from . import rdkit, formula_annotation, formats, hrms_core
 
 if __name__ == "__main__":
     print("This is the hrms_utils package. Import submodules to use functionalities.")
--- src/hrms_utils/hrms_core/__init__.py
+++ src/hrms_utils/hrms_core/__init__.py
@@ -8,13 +8,13 @@ from typing import TYPE_CHECKING
 import polars as pl
 from polars.plugins import register_plugin_function
 
-from ._internal import __version__ as __version__
-from ._internal import NUM_ELEMENTS
+from .._internal import __version__ as __version__
+from .._internal import NUM_ELEMENTS
 
 if TYPE_CHECKING:
     from .typing import IntoExprColumn
 
-LIB = Path(__file__).parent
+LIB: Path = Path(__file__).parent.parent
 
 # Placeholder for the Polars expression namespace
 @pl.api.register_expr_namespace("mass_decomposition")
